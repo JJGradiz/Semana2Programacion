daba888869551f341a2e730c98e3574da866fc26
Jeremy Gradiz <jjgradiz@uamv.edu.ni>
2026-08-27 18:39:38 -0600

    Delete src/main directory
    eliminacion de archivo basura

--- src/main/java/ni/edu/uam/registro_app/Interfaces/Grud.java
+++
@@ -1,9 +0,0 @@
-package ni.edu.uam.registro_app.Interfaces;
-
-import java.util.List;
-
-public interface Grud <T> {
-    public void agregar(T entidad);
-    public List<T> Obtener();
-
-}
--- src/main/java/ni/edu/uam/registro_app/Launcher.java
+++
@@ -1,9 +0,0 @@
-package ni.edu.uam.registro_app;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
